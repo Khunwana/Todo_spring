8ca8633f8f81def46805edd7b5c7ecf00080b343
Monias <khunwana.monias@gmail.com>
2024-04-21 01:12:22 +0200

    Authentication class for names and password input authentication
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/devkhunwana/springboot/toDoApp/login/AuthenticationService.java
@@ -0,0 +1,15 @@
+package com.devkhunwana.springboot.toDoApp.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+	public boolean authenticate(String username, String password) {
+		
+		boolean isValidUserName = username.equalsIgnoreCase("monias");
+		boolean isValidPassword = password.equalsIgnoreCase("1234");
+		
+		return isValidUserName && isValidPassword;
+	}
+}
