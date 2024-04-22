7e4ba643724b3a48f2d02103f2f5ac7c21d86668
Monias <khunwana.monias@gmail.com>
2024-04-22 02:33:41 +0200

    Additions of application fields Class, Controller and hardCoded items in service class
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/TodoController.java
@@ -0,0 +1,24 @@
+package com.devkhunwana.springboot.toDoApp.appImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TodoController {
+	@Autowired
+	private TodoService todoService;
+	
+	
+	@RequestMapping("list-todos")
+	public String listAllTodos(ModelMap model)
+	{
+		List<Todo> todos = todoService.findByUsername("Monias");
+		model.addAttribute("todos", todos);
+		return "listTodos";
+	}
+	
+}
---
+++ src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/TodoService.java
@@ -0,0 +1,24 @@
+package com.devkhunwana.springboot.toDoApp.appImpl;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	private static ArrayList<Todo> todos;
+	
+	static {
+		todos= new ArrayList<Todo>();
+		todos.add(new Todo(1,"Monias", "master Springboot",LocalDate.now().plusMonths(1),false));
+		todos.add(new Todo(1,"Monias", "master SpringSecurity",LocalDate.now().plusMonths(1),false));
+		todos.add(new Todo(1,"Monias", "master FullStack dev",LocalDate.now().plusMonths(1),false));
+	}
+	
+	public List<Todo> findByUsername(String username)
+	{
+		return todos;
+	}
+}
