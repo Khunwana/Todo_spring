1bd5403e46d6b36772e9d565a9750a98305100f6
Monias <khunwana.monias@gmail.com>
2024-04-23 23:49:40 +0200

    Implementation of add&submit methods for redirecting to list&add pages for To-Do
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/TodoController.java
+++ src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/TodoController.java
@@ -1,11 +1,14 @@
 package com.devkhunwana.springboot.toDoApp.appImpl;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -14,13 +17,30 @@ public class TodoController {
 	@Autowired
 	private TodoService todoService;
 	
-	
+
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model)
 	{
 		List<Todo> todos = todoService.findByUsername("Monias");
 		model.addAttribute("todos", todos);
 		return "listTodos";
+	}
+
+	@RequestMapping(value="add-todo",method = RequestMethod.GET)
+	public String addTodos()
+	{
+//		List<Todo> todos = todoService.findByUsername("Monias");
+//		model.addAttribute("todos", todos);
+		return "addTodo";
+	}
+	
+	@RequestMapping(value="add-todo",method = RequestMethod.POST)
+	public String submitTodos(@RequestParam String username,@RequestParam String description)
+	{
+//		List<Todo> todos = todoService.findByUsername("Monias");
+//		model.addAttribute("todos", todos);\
+		todoService.addnewTodo(username, description, LocalDate.now().plusYears(1), false);
+		return "redirect:list-todos";
 	}
 	
 }
