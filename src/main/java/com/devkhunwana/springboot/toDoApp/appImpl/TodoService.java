ed06f09285ab2e0bae2f675b689fb378b2d1f5c4
Monias <khunwana.monias@gmail.com>
2024-04-23 23:51:18 +0200

    add new todo method for adding new to-do to arraylist
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/TodoService.java
+++ src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/TodoService.java
@@ -8,17 +8,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
-	private static ArrayList<Todo> todos;
-	
+	private static ArrayList<Todo> todos= new ArrayList<Todo>();;
+	private static int todoCount = 0;
+
 	static {
-		todos= new ArrayList<Todo>();
-		todos.add(new Todo(1,"Monias", "master Springboot",LocalDate.now().plusMonths(1),false));
-		todos.add(new Todo(2,"Monias", "master SpringSecurity",LocalDate.now().plusMonths(1),false));
-		todos.add(new Todo(3,"Monias", "master FullStack dev",LocalDate.now().plusMonths(1),false));
+		
+		todos.add(new Todo(++todoCount,"Monias", "master Springboot",LocalDate.now().plusMonths(1),false));
+		todos.add(new Todo(++todoCount,"Monias", "master SpringSecurity",LocalDate.now().plusMonths(1),false));
+		todos.add(new Todo(++todoCount,"Monias", "master FullStack dev",LocalDate.now().plusMonths(1),false));
 	}
 	
 	public List<Todo> findByUsername(String username)
 	{
 		return todos;
+	}
+	
+	public void addnewTodo(String username, String description, LocalDate targetDate, boolean done)
+	{
+		todos.add(new Todo(++todoCount,username,description,targetDate,done));
 	}
 }
