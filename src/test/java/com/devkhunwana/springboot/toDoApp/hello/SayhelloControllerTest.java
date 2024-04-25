90b051382003abdd055164f8b2fea937e7241ab3
Monias <khunwana.monias@gmail.com>
2024-04-25 16:06:36 +0200

    testing sayHelloController methods
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/test/java/com/devkhunwana/springboot/toDoApp/hello/SayhelloControllerTest.java
@@ -0,0 +1,53 @@
+package com.devkhunwana.springboot.toDoApp.hello; 
+  
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc; 
+  
+@RunWith(SpringRunner.class) 
+@WebMvcTest(SayHelloController.class) 
+class SayhelloControllerTest { 
+  
+    @Autowired
+    private MockMvc mockMvc; 
+  
+    @Test
+    public void checkSuccessMessage() throws Exception 
+    { 
+        this.mockMvc 
+            .perform(get("/say-hello")) 
+            .andExpect(status().isOk()) 
+            .andExpect(content().string( 
+                containsString("hello! What are you learning today?")));
+        System.out.println(content().toString());
+    } 
+    
+    @Test
+    public void checkHtmlMessage() throws Exception 
+    { 
+        this.mockMvc 
+            .perform(get("/say-hello-html")) 
+            .andExpect(status().isOk()) 
+            .andExpect(content().string( 
+                containsString("My First Html page with body")));
+//        System.out.println(content().toString());
+    } 
+    
+    @Test
+    public void checkJspMessage() throws Exception 
+    { 
+        this.mockMvc 
+            .perform(get("/say-hello-jsp")) 
+            .andExpect(status().isOk()) ;
+//            .andExpect(content().)
+//        System.out.println(content().toString());
+    } 
+}
