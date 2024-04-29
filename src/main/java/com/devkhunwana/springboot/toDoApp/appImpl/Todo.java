eddb7e3968c534e49bbbdb23ae0aa06aeedf838c
Monias <khunwana.monias@gmail.com>
2024-04-30 00:03:11 +0200

    connecting class to db as @Entity
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/Todo.java
+++ src/main/java/com/devkhunwana/springboot/toDoApp/appImpl/Todo.java
@@ -2,10 +2,13 @@ package com.devkhunwana.springboot.toDoApp.appImpl;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 
-
+@Entity
 public class Todo {
 
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
@@ -16,8 +19,11 @@ public class Todo {
 		this.targetDate = targetDate;
 		this.done = done;
 	}
-
+	
+	@Id
+	@GeneratedValue
 	private int id;
+	
 	private String username;
 	
 	@Size(min=10, message="Enter atleast 10 characters")
