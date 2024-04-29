63e1ff5ea1bae87be7c5d058875a964b5310914c
Monias <khunwana.monias@gmail.com>
2024-04-30 00:01:57 +0200

    sql data insertion
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into todo (DONE, ID, TARGET_DATE, DESCRIPTION, USERNAME)
+values (false, 10001, CURRENT_DATE(), 'Learn springframework', 'monias');
+insert into todo (DONE, ID, TARGET_DATE, DESCRIPTION, USERNAME)
+values (false, 10002, CURRENT_DATE(), 'Learn spring boot', 'monias');
+insert into todo (DONE, ID, TARGET_DATE, DESCRIPTION, USERNAME)
+values (false, 10003, CURRENT_DATE(), 'Learn spring mvc', 'monias');
