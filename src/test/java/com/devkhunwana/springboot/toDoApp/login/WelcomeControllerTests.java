0e4fd47c677680f9ee8f231bc3e971c8519da30f
Monias <khunwana.monias@gmail.com>
2024-05-06 13:27:56 +0200

    updated tests
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/test/java/com/devkhunwana/springboot/toDoApp/login/WelcomeControllerTests.java
+++ src/test/java/com/devkhunwana/springboot/toDoApp/login/WelcomeControllerTests.java
@@ -1,18 +1,30 @@
-package com.devkhunwana.springboot.toDoApp.login; 
+package com.devkhunwana.springboot.toDoApp.login;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(WelcomeController.class) 
-class ToDoControllerTests { 
-  
-  
-    @Test
-    public void checkSuccessMessage() throws Exception 
-    { 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-    } 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WelcomeControllerTests {
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+		
+	@Test
+	public void checkLogin() throws Exception
+	{
+		ResponseEntity<String> response = restTemplate
+				.withBasicAuth("monias", "1234")
+				.getForEntity("/login", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
 }
